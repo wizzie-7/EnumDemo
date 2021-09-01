1b6fdeb312676f23c5d07902db2565248ceffc82
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 20:48:26 +0530

    Add project files.

---
+++ EnumEx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    class EnumEx
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
