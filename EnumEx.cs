8d0b34fe5004080bfca2ecf7962b470354feff32
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 20:58:01 +0530

    first commit

--- EnumEx.cs
+++ EnumEx.cs
@@ -4,9 +4,44 @@ namespace EnumDemo
 {
     class EnumEx
     {
+        enum Days
+        {
+            sun, mon, tue, wed, thus, fri, sat
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Days days = Days.sat;
+            switch(days)
+            {
+                case Days.sun:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("PUBG Day");
+                    break;
+                case Days.mon:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("BGMI Day");
+                    break;
+                case Days.tue:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("Valorant Day");
+                    break;
+                case Days.wed:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("MindCraft Day");
+                    break;
+                case Days.thus:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("CS:GO Day");
+                    break;
+                case Days.fri:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("GTA5-Story mode Day");
+                    break;
+                case Days.sat:
+                    Console.WriteLine("Which Game Day Today?");
+                    Console.WriteLine("GTA5-RP mode Day");
+                    break;
+            }
         }
     }
 }
